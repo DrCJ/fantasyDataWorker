051c42916f2b4981a3be66c9118b3c76cfb536b4
DookPham <dookpham@gmail.com>
2016-08-09 16:05:19 -0700

    Update url for get all stats in one week

--- getFantasyDataStats.js
+++ getFantasyDataStats.js
@@ -2,6 +2,11 @@ const request = require('request');
 const db = require('./db_config.js');
 const { fantasydata } = require('./getFantasyDataStats');
 
+
+//this is url for all player projections in one week
+//https://api.fantasydata.net/v3/nfl/projections/{format}/PlayerGameProjectionStatsByWeek/{season}/{week}
+
+//this is test url for 2016 week 1 atlanta only
 const url = 'https://api.fantasydata.net/v3/nfl/projections/JSON/PlayerGameProjectionStatsByTeam/2016/1/ATL';
 request({
   headers: {
